142e61ee5b4cf3123cc9d676ebce43f95ac77277
duymanh3602 <82275607+duymanh3602@users.noreply.github.com>
2024-01-10 13:38:10 +0700

    fix: test

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
 # Awesome Project Build with TypeORM
 
-Steps to run this project:
+~~Steps to run this project:~~ **deprecated***
 
 1. Run `npm i` command
 2. Setup database settings inside `data-source.ts` file
 3. Run `npm start` command
+-----------------------------------------------------
+Update .env
+Install dependencies ```yarn```
+Run scripted ```yarn start```
+
+-----------------------------------------------------
+Estimated results (local)
+Specs: 
++ CPU: i5-1135G7
++ Ram: 16Gb 3200Mhz
+
+Gen test MySQL 100000 records : 96.54 (s)
+Transform 100000 records from MySQL to MongoDB: ~50 (s)
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,20 @@
 import { MySQLDataSource } from "./core/mysql-connection"
-import { Questions } from "./entity/Questions"
 import { bulkQuestion } from "./core/mongo-connection";
-import { kill } from "process";
+import { Questions } from "./entity/Questions"
+
 
 const questionRepository = MySQLDataSource.getRepository(Questions);
 
 const worker = async (index: number) => {
-    const questions = await questionRepository.find({
+    await questionRepository.find({
         take: 1000,
         skip: index
     })
-    questions.forEach((question) => {
-        bulkQuestion(question);
-    });
+    .then(async (questions) => {
+        questions.forEach( async (question) => {
+            await bulkQuestion(question);
+        });
+    })
 }
 
 const loadData = async () => {
@@ -23,19 +25,23 @@ const loadData = async () => {
             if (dataLength < 1) {
                 console.log("Empty target table");
                 process.exit();
-
             }            
             let promiseAll = [];
             for (var i = 0; i <= dataLength / 1000; i++) {                
-                promiseAll.push(worker(i))
+                promiseAll.push(await worker(i)
+                    .then(() => {
+                        console.log("Imported " + i);
+                        }
+                    )
+                )
             }            
             Promise.all(promiseAll).then(() => {
                 const now = performance.now();
                 console.log(`DONE w/ ${now - start} (ms) estimated for ${dataLength} ${dataLength === 1 ? 'record' : 'records'}`);
-                process.exit();
+                // process.exit();
             }).catch((err) => {
                 console.log(err);
-            });            
+            });
         } catch(err) {
             console.log(err);
         }
