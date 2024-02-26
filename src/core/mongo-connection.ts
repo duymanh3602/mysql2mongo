60770255ca65b31a8921f4ce7870c1e07a481a0d
Manh Dang <manhdvvnu@gmail.com>
2024-02-26 17:29:46 +0700

    feat: update script

--- src/core/mongo-connection.ts
+++ src/core/mongo-connection.ts
@@ -1,44 +1,48 @@
-import { UUID } from 'bson';
-import { Schema, model, connect, Date } from 'mongoose';
-import { Questions } from '../entity/Questions';
+import { UUID } from "bson";
+import { Schema, model, connect, Date } from "mongoose";
+import { Questions } from "../entity/Questions";
 
-const CONNECT_STRING = process.env.MONGO_CONNECT_STRING ?? '';
-const COLLECTION_NAME = process.env.MONGO_COLLECTION_NAME ?? 'questions';
-
+const CONNECT_STRING = process.env.MONGO_CONNECT_STRING ?? "";
+const COLLECTION_NAME = process.env.MONGO_COLLECTION_NAME ?? "questions";
+const DATABASE_NAME = process.env.MONGO_DATABASE_NAME ?? "hoclieu";
 // 1. Create an interface representing a document in MongoDB.
 interface IQuestion {
-    QuestionId: UUID
-    DataHash: string
-    Content: string
-    Remember: string
-    Solve: string
-    CorrectAnswer: string
-    CreatedDate: Date
-    CreatedBy?: UUID
-    ModifiedDate: Date
-    ModifiedBy?: UUID
+  QuestionId: UUID;
+  DataHash: string;
+  Content: any;
+  Remember: any;
+  Solve: any;
+  CorrectAnswer: any[];
+  CreatedDate: Date;
+  CreatedBy?: UUID;
+  ModifiedDate: Date;
+  ModifiedBy?: UUID;
 }
 
 // 2. Create a Schema corresponding to the document interface.
-const questionSchema = new Schema<IQuestion>({
+const questionSchema = new Schema<IQuestion>(
+  {
     QuestionId: { type: UUID, required: false },
     DataHash: { type: String, required: false },
-    Content: { type: String, required: false },
-    Remember: { type: String, required: false },
-    Solve: { type: String, required: false },
-    CorrectAnswer: { type: String, required: false },
+    Content: { type: Object, required: false },
+    Remember: { type: Object, required: false },
+    Solve: { type: Object, required: false },
+    CorrectAnswer: { type: [], required: false },
     CreatedDate: { type: Date, required: false },
     CreatedBy: { type: String, required: false },
     ModifiedDate: { type: Date, required: false },
     ModifiedBy: { type: String, required: false },
-}, { versionKey: false, collection: COLLECTION_NAME });
+  },
+  { versionKey: false, collection: COLLECTION_NAME }
+);
 
 // 3. Create a Model.
-export const Question = model<IQuestion>('', questionSchema);
+export const Question = model<IQuestion>("", questionSchema);
 
 const options = {
   connectTimeoutMS: 100000,
   socketTimeoutMS: 100000,
+  dbName: DATABASE_NAME,
 };
 
 export async function bulkQuestion(questions: Questions[]) {
@@ -46,19 +50,47 @@ export async function bulkQuestion(questions: Questions[]) {
   try {
     await connect(CONNECT_STRING, options);
 
-    await Promise.all(
-      questions.map(async (q) => {
-        if (!q.IsUpdate) {
-          const question = new Question({
-            ...q,
-            IsUpdate: true,
-            QuestionId: q.Id,
-          });
-          await question.save();
-        }
-      })
-    );
+    let promises = [];
+    questions.map(async (q) => {
+      // if (!q.IsUpdate) {
+      // find question by id
+      // let ques = await Question.findOne({ Question, DataHash: q.DataHash }).exec();
+
+      const question = new Question({
+        ...q,
+        IsUpdate: true,
+        CorrectAnswer: deserializeListDynamic(q.CorrectAnswer),
+        Content: JSON.parse(q.Content),
+        Remember: JSON.parse(q.Remember),
+        Solve: JSON.parse(q.Solve),
+        QuestionId: q.Id,
+      });
+      promises.push(question.save());
+      // }
+    });
+    await Promise.all(promises);
   } catch (err) {
     console.log(err);
   }
 }
+
+// c# code convert to typescript
+// private static List<dynamic> DeserializeListDynamic(string v)
+// {
+//     v = "{\"Result\":" + v + "}";
+//     dynamic obj = JsonConvert.DeserializeObject<ExpandoObject>(v, new ExpandoObjectConverter());
+//     return obj.Result;
+// }
+
+// private static dynamic DeserializeDynamic(string v)
+// {
+//     v = "{\"Result\":" + v + "}";
+//     dynamic obj = JsonConvert.DeserializeObject<ExpandoObject>(v, new ExpandoObjectConverter());
+//     return obj.Result;
+// }
+
+const deserializeListDynamic = (v: string) => {
+  v = '{"Result":' + v + "}";
+  let obj = JSON.parse(v);
+  return obj.Result;
+};
--- src/index.ts
+++ src/index.ts
@@ -1,108 +1,112 @@
-import { MySQLDataSource } from "./core/mysql-connection"
+import { MySQLDataSource } from "./core/mysql-connection";
 import { bulkQuestion } from "./core/mongo-connection";
-import { Questions } from "./entity/Questions"
-import { IsNull } from "typeorm";
+import { Questions } from "./entity/Questions";
+import { Between } from "typeorm";
 
-const BATCH_SIZE = parseInt(process.env.BATCH_SIZE ?? '1000');
+const BATCH_SIZE = parseInt(process.env.BATCH_SIZE ?? "1000");
+const DATE_FILTER = process.env.DATE_START ?? undefined;
+var fs = require("fs");
 
 const questionRepository = MySQLDataSource.getRepository(Questions);
 
-let stopFlag = false;
-let bulkFlag = false;
-let updateFlag = false;
-
-const myCallback = () => {
-    console.log('Done!');
-    process.exit();
-}
-  
-
-const update = async (questions: Questions[]) => {
-    questions.forEach(async (question) => {
-        // if ( question.IsUpdate === true ) return;
-        // else {
-            const data = {
-                ...question,
-                IsUpdate: true
-            }
-            await questionRepository.save(data);
-        // }
-    });
-}
-
-const worker = async (index: number) => {
-    bulkFlag = false;
-    updateFlag = false;
-    const questions = await questionRepository.find({
-        where: { IsUpdate: IsNull() },
-        take: BATCH_SIZE,
-        skip: index * BATCH_SIZE
-    });
-
-    await bulkQuestion(questions).then(() => {
-        bulkFlag = true;
-    });
-    await update(questions).then(() => {
-        updateFlag = true;
-    });
-}
-
 const loadData = async () => {
-    console.log('Press "q" to stop ...');
-    
-    await MySQLDataSource.initialize().then(async () => {
-        try {
-            const dataLength = await questionRepository.count({ where: { IsUpdate: IsNull() } });
-            console.log(`Ready to import ${dataLength} ${dataLength === 1 ? 'record' : 'records'}`);
-            
-            if (dataLength < 1) {
-                console.log("Empty target table");
-                process.exit();
-            }
+  console.log("Loading data...");
+  console.log('Press "q" to stop ...');
+  await MySQLDataSource.initialize().then(async () => {
+    let timeFilter = new Date();
+    try {
+      if (DATE_FILTER) {
+        timeFilter = new Date(DATE_FILTER);
+      } else {
+        let questionOldest = await questionRepository.findOne({
+          order: { CreatedDate: "ASC" },
+          select: ["CreatedDate"],
+          where: {},
+        });
+        timeFilter = new Date(questionOldest?.CreatedDate ?? new Date());
+      }
+      // dateLowerLimitFilter format yyyy-mm-dd 00:00:00
+      let dateLowerLimitFilter = new Date(timeFilter.getTime());
+      dateLowerLimitFilter.setHours(0, 0, 0, 0);
+      // dateUpperLimitFilter format yyyy-mm-dd 23:59:59
+      let dateUpperLimitFilter = new Date(timeFilter.getTime());
+      dateUpperLimitFilter.setHours(23, 59, 59, 999);
+      console.log(
+        "Start update from " +
+          dateLowerLimitFilter.toLocaleDateString() +
+          " " +
+          dateLowerLimitFilter.toLocaleTimeString() +
+          " to " +
+          dateUpperLimitFilter.toLocaleDateString() +
+          " " +
+          dateUpperLimitFilter.toLocaleTimeString()
+      );
 
-            let promiseAll = [];
-            for (let i = 0; i <= dataLength / BATCH_SIZE; i++) {
-                if (stopFlag) break;
-                promiseAll.push(await worker(i)
-                    .then(() => {
-                        console.log("Loaded " + i);
-                    })
-                )
-            }
-            
-            Promise.all(promiseAll)
-                .then(() => {
-                    !stopFlag && console.log(`Imported ${dataLength} ${dataLength === 1 ? 'record' : 'records'}`);
-                    if (bulkFlag && updateFlag) {
-                        console.log('Waiting for 10 seconds to exit ...');
-                        
-                        const delay = 60000;
-                        // Wait for 60 seconds to complete all await processes before exit the program 
-                        setTimeout(myCallback, delay);
-                    }
-                })
-                .catch((err) => {
-                    console.log(err);
-                    process.exit(1);
+      let flag = true;
+      while (flag) {
+        // update IsUpdate = null fon dateFilter
+        let questions = await questionRepository.count({
+          where: {
+            CreatedDate: Between(dateLowerLimitFilter, dateUpperLimitFilter),
+          },
+        });
+
+        if (questions > 0) {
+          for (let i = 0; i < questions; i += BATCH_SIZE) {
+            let questions = await questionRepository.find({
+              where: {
+                CreatedDate: Between(
+                  dateLowerLimitFilter,
+                  dateUpperLimitFilter
+                ),
+              },
+              take: BATCH_SIZE,
+              skip: i,
             });
-        } catch(err) {
-            console.log(err);
+
+            await bulkQuestion(questions).then(() => {});
+          }
+          console.log(
+            "Update done " +
+              questions +
+              " records in " +
+              "s. Date" +
+              dateLowerLimitFilter.toLocaleDateString()
+          );
+          // write log in file output.txt
+          fs.appendFileSync(
+            "src/output.txt",
+            "Update done " +
+              questions +
+              " records in " +
+              "s. Date" +
+              dateLowerLimitFilter.toLocaleDateString() +
+              "\n"
+          );
         }
-    })
-}
+
+        // increment dateLowerLimitFilter
+        dateLowerLimitFilter.setDate(dateLowerLimitFilter.getDate() + 1);
+        dateUpperLimitFilter.setDate(dateUpperLimitFilter.getDate() + 1);
+        continue;
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  });
+};
 
 const receiveButtonPress = () => {
-    process.stdin.setRawMode(true);
-    process.stdin.resume();
-    process.stdin.on('data', (data) => {
-        const input = data.toString().trim();
-        if (input === 'Q' || input === 'q') {
-            stopFlag = true;
-            console.log('Button press received. Stopping all await processes...');
-        }
-    });
-}
+  process.stdin.setRawMode(true);
+  process.stdin.resume();
+  process.stdin.on("data", (data) => {
+    const input = data.toString().trim();
+    if (input === "Q" || input === "q") {
+      console.log("Button press received. Stopping all await processes...");
+      process.exit();
+    }
+  });
+};
 
 receiveButtonPress();
 loadData();
-
